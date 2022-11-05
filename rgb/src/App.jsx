a31ac8dca2dcf42765b77df035163ff002c3af94
Zhangjason308 <zhangjason308@gmail.com>
2022-11-05 15:20:05 -0400

    HI

--- rgb/src/App.jsx
+++ rgb/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
 import Workout from './components/workout/Workout'
 import LiveVid from './components/livevid/LiveVid'
 import Footer from './components/footer/Footer'
@@ -8,9 +7,7 @@ import Footer from './components/footer/Footer'
 const App = () => {
   return (
   <>
-  {/* <React /> */}
   <Header /> 
-  <Nav />
   <Workout />
   <LiveVid />
   <Footer />
--- rgb/src/components/header/Header.jsx
+++ rgb/src/components/header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../assets/logo.png';
 const Header = () => {
   return (
     <header>
-    {/* <div>Header</div> */}
-    <div class = "headerDiv">
-      <h2>Test Header</h2>
+    
+    {/* <div className="container header__container">
+    <h1>Rep Gym Bro</h1>
+    </div> */}
+    <div className="header_logo">
+    <img src={logo} class="logo" alt="Logo isn't working" />
+    </div>
+    <div className="header_background">
+    <h1 class="header_colour"></h1>
     </div>
     </header>
   )

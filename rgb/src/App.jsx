813f0c8ed4fbd7491b2df6d49723c943aee0b1a3
Zhangjason308 <zhangjason308@gmail.com>
2022-11-05 13:39:50 -0400

    added initial jsx and css files

---
+++ rgb/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Workout from './components/workout/Workout'
+import LiveVid from './components/livevid/LiveVid'
+import Footer from './components/footer/Footer'
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+  <>
+  <React />
+  <Header /> 
+  <Nav />
+  <Workout />
+  <LiveVid />
+  <Footer />
+  </>
+    
+  )
+}
+
+export default App
---
+++ rgb/src/components/livevid/Livevid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Livevid = () => {
+  return (
+    <div>Livevid</div>
+  )
+}
+
+export default Livevid
